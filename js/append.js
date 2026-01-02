87cd794d4bae5a7de5a348d8b48bff59370d2fe1
Mvittorelli <mvittorelli@edenschool.fr>
2026-01-02 10:41:09 +0100

    Add custom cursors and expand 'more' section content
    Introduced custom cursor files and updated CSS to use them for various elements. Enhanced the 'more' section with additional personal information about music, maximalism, curiosity, and gaming interests. Also updated the music section header and added a Webring button to the homepage.

--- js/append.js
+++ js/append.js
@@ -97,9 +97,6 @@ const contact = `<div id="contact">
         </div>`;
 const more = `<section class="moreWelc">
           <h3>° ~ Tu veux savoir plus de choses sur moi ?</h3>
-          <p>
-            <span class="bold">Bien on va commencer par mes passions</span>.
-          </p>
           <h3>Fou de sport</h3>
           <p>
             Je fais pas mal de sport, que ce soit de l'escalade en bloc, du
@@ -122,6 +119,33 @@ const more = `<section class="moreWelc">
               alt="vielle photo de moi"
             />
           </div>
+          <h3>Fan de musique</h3>
+          <p>
+            J'écoute de tout, scene/scenecore, hyperpop, electro, hardstyle,
+            tekk, industrial hardcore, nightcore, rock, métal, vocaloid ...
+            j'aime vraiment tout. (tu peu utiliser la playlist pour te faire une
+            idée)
+          </p>
+          <h3>Maximaliste</h3>
+          <p>
+            Tu l'aura sûrement remarqué, mais je ne suis pas du genre
+            minimaliste. J'aime ajouter une multitude de détails à mes projets.
+          </p>
+          <h3>Touche à tout</h3>
+          <p>
+            Je suis quelqu'un d'êxtremement curieux, avec une passion par jours,
+            je me retrouve à apprendre plein de choses sur un peu tout les
+            domaines.
+          </p>
+          <h3>Jeux vidéos</h3>
+          <p>
+            Comme tout le monde, je joue à pas mal de jeux, la aussi je suis
+            assez touche à tout, car j'aime autant les jeux de simulation
+            millitaire (arma reforger, hell let looser, war thunder), que les
+            jeux de rythmes (osu!, vivid/stasis, project sekai), des jeux de
+            survie / aventure (raft, green hell, minecraft), ou des petits jeux
+            aléatoire entre amis (one armed cook, on armed robber, dark hours)
+          </p>
         </section>`;
 
 const left = document.getElementById("left");
